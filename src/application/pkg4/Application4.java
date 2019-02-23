1614613bd158d33fd2db06160956ff96677d6d73
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-02-23 14:25:46 +0530

    application4
    application4

---
+++ src/application/pkg4/Application4.java
@@ -0,0 +1,18 @@
+
+package application.pkg4;
+
+
+public class Application4 {
+
+    
+    public static void main(String[] args) {
+       int num=8;
+       if (num>10){
+           System.out.println("Greater than 10");
+       }
+       else{
+       System.out.println("Less than 10");
+    
+    }
+    
+}
